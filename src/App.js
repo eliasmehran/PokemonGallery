4561de7d9d607445b6946bd8143c58d34b875838
Elias Mehran <elias.mehran@stud.kbw.ch>
2022-05-11 15:01:05 +0200

    added video

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import Carousel from "react-gallery-carousel";
 import 'react-gallery-carousel/dist/index.css';
+import './App.css';
+import PropTypes from "prop-types";
 
+const YoutubeEmbed = ({ embedId }) => (
+    <div className="video">
+        <iframe
+            width="853"
+            height="480"
+            src={`https://www.youtube.com/embed/${embedId}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+        />
+    </div>
+);
+
+YoutubeEmbed.propTypes = {
+    embedId: PropTypes.string.isRequired
+};
 
 const App = () => {
 
@@ -14,8 +33,10 @@ const App = () => {
     }
 
     return (
-        <div>
-            <Carousel images={images} style={{height: 750, width: 750}}/>
+        <div className="App">
+            <h1>Elias M152</h1>
+            <Carousel className="Gallery" images={images} style={{height: 700, width: 700, margin: 15}}/>
+            <YoutubeEmbed embedId="Gmhh6Ppb16k" />
         </div>
     );
 }
